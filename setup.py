a9615c58d9fbb992ef214423829d75c1815fcf10
Nagarjuna Kumarappan <nagarjuna.kumarappan@jagex.com>
2015-11-16 13:55:17 +0000

    fixed minor typo in versioning

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def read(filename):
 LONG_DESC = read('README.md') + '\nCHANGES\n=======\n\n' + read('CHANGES')
 
 setup(name='pyfzf',
-      version=read('VERSION'),
+      version=read('VERSION').strip('\n'),
       description="Python wrapper for junegunn's fuzzyfinder (fzf)",
       long_description=LONG_DESC,
       author='Nagarjuna Kumarappan',
